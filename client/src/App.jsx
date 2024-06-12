23af59ac45f5af3f6d1fb29d878b3398fdc0e273
Shantanu Pawar <pawarshantanu58@gmail.com>
2024-06-12 16:05:37 +0530

    Install required packages react tailwind, create header section and create a server using express

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import { Header } from './components/Header'
+
+function App() {
+  return (
+
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path='/'element={<Home/>}></Route>
+    <Route path='/about'element={<About/>}></Route>
+    <Route path='/profile'element={<Profile/>}></Route>
+    <Route path='/sign-in'element={<SignIn/>}></Route>
+    <Route path='/sign-up'element={<SignUp/>}></Route>
+  </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
